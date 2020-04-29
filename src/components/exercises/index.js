a737f118808b889e98452b72a0c9a3352ababb2b
Michael Greco <michael.greco07@gmail.com>
2020-04-29 09:37:20 -0400

    grid layout

--- src/components/exercises/index.js
+++ src/components/exercises/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import Left from './Left';
+import Right from './Right';
+
+const styles = {
+  Paper: { padding: 20, marginTop: 10, marginBottom: 10 },
+};
 
 export default (props) => (
-  <div>
-    <h1>hello from exercises</h1>
-  </div>
+  <Grid container>
+    <Grid item sm>
+      <Left styles={styles}></Left>
+    </Grid>
+    <Grid item sm>
+      <Right styles={styles}></Right>
+    </Grid>
+  </Grid>
 );
